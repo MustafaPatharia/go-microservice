3f387b052c94b423236c2b60688ab32f286bf682
Mustafa Patharia <patharia52@gmail.com>
2025-07-13 11:26:10 +0400

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/MustafaPatharia/go-microservice
+
+go 1.24.5
+
+require (
+	github.com/go-chi/chi/v5 v5.2.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	router := chi.NewRouter()
+
+	router.Get("/hello", basicHandler)
+
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: router,
+	}
+
+	fmt.Println("Server started on :3000")
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Printf("Error starting server: %v\n", err)
+	}
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
+}
