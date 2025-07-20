3ba25fe93456f2c447ebe5b706d23d5a6e8bfa28
Mustafa Patharia <patharia52@gmail.com>
2025-07-20 13:05:32 +0400

    Handled Shutdown

--- main.go
+++ main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/MustafaPatharia/go-microservice/application"
 )
 
 func main() {
 	app := application.New() // app is a pointer to App (memory address)
-	err := app.Start(context.TODO())
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Start(ctx)
 
 	if err != nil {
 		fmt.Printf("Error starting application: %v\n", err)
